c555a63d75364b75c35a26a1f438af6e8b25e61a
Arthas <420171312@qq.com>
2020-06-20 12:04:19 +0800

    Q26_SubstructureInTree 树的子结构 done

---
+++ Q26_SubstructureInTree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q26_SubstructureInTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    class Solution
+    {
+        public bool HasSubtree(TreeNode pRoot1, TreeNode pRoot2)
+        {
+            if (pRoot1 == null || pRoot2 == null)
+            {
+                return false;
+            }
+
+            bool result = false;
+
+            if (pRoot1.val == pRoot2.val)
+            {
+                result = DoesAHaveB(pRoot1, pRoot2);
+            }
+
+            if (!result)
+            {
+                result = HasSubtree(pRoot1.left, pRoot2);
+            }
+
+            if (!result)
+            {
+                result = HasSubtree(pRoot1.right, pRoot2);
+            }
+
+            return result;
+        }
+
+        public bool DoesAHaveB(TreeNode rootA, TreeNode rootB)
+        {
+            if (rootB == null)
+            {
+                return true;
+            }
+
+            if (rootA == null)
+            {
+                return false;
+            }
+
+            bool result = false;
+
+            if (rootA.val == rootB.val)
+            {
+                result = DoesAHaveB(rootA.left, rootB.left) && DoesAHaveB(rootA.right, rootB.right);
+            }
+
+            return result;
+        }
+    }
+}
