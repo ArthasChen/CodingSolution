9754dea346dca278b7741b3444fb1acdea3d14b9
Arthas <420171312@qq.com>
2020-06-23 01:03:16 +0800

    Q37_SerializeBinaryTrees 序列化二叉树 done

--- Q37_SerializeBinaryTrees/Program.cs
+++ Q37_SerializeBinaryTrees/Program.cs
@@ -10,6 +10,26 @@ namespace Q37_SerializeBinaryTrees
     {
         static void Main(string[] args)
         {
+            // Creat a tree
+            TreeNode node1 = new TreeNode(1);
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node7 = new TreeNode(7);
+
+            node1.left = node2;
+            node1.right = node3;
+            node2.left = node4;
+            node3.right = node7;
+
+
+            string inputString = "1!2!4!#!#!#!3!#!7!#!#!";
+
+            Solution s = new Solution();
+            var head = s.Serialize(node1);
+
+            Console.ReadKey();
+
         }
     }
 
@@ -38,20 +58,46 @@ namespace Q37_SerializeBinaryTrees
 
     class Solution
     {
+        StringBuilder sb = new StringBuilder();
         public string Serialize(TreeNode root)
         {
             // write code here
+            if (root == null)
+            {
+                return "#!";
+            }
+
+            PreOrderTraversalWithRecursive(root);
+
+
+            return sb.ToString();
+        }
+
+        public void PreOrderTraversalWithRecursive(TreeNode root)
+        {
+            if (root == null)
+            {
+                sb.Append("#!");
+                return;
+            }
+
+            sb.Append(root.val);
+            sb.Append("!");
+            //Console.WriteLine(root.val.ToString() + "  ");
+            PreOrderTraversalWithRecursive(root.left);
+            PreOrderTraversalWithRecursive(root.right);
         }
 
         public TreeNode Deserialize(string str)
         {
             // write code here
 
-            string[] inputCharArray = str.Split('!');
+            string[] inputCharArray = str.Split(new char[1] { '!' }, StringSplitOptions.RemoveEmptyEntries);
 
             ReturnType resultTreeNode = null;
 
             //str.
+            return DeserializeRecursive(inputCharArray).treeNode;
         }
 
         public ReturnType DeserializeRecursive(string[] strArray)
@@ -66,20 +112,49 @@ namespace Q37_SerializeBinaryTrees
             string rootStr = strArray[0];
             if (rootStr != "#")
             {
-                string[] newStrArray = new string[strArray.Length - 1];
-                strArray.CopyTo(newStrArray, 1);
+                int newStrArrayLength = strArray.Length - 1;
 
-                ReturnType leftRT = DeserializeRecursive(newStrArray);
-                ReturnType rightRT = DeserializeRecursive(leftRT.strArray);
+                if (newStrArrayLength > 0)
+                {
+                    string[] newStrArray = new string[strArray.Length - 1];
+                    for (int i = 1; i < strArray.Length; i++)
+                    {
+                        newStrArray[i - 1] = strArray[i];
+                    }
+                    //strArray.CopyTo(newStrArray, 0);
 
-                TreeNode curTreeNode = new TreeNode(int.Parse(rootStr));
-                curTreeNode.left = leftRT.treeNode;
-                curTreeNode.right = rightRT.treeNode;
-                rt = new ReturnType(curTreeNode, newStrArray);
+                    ReturnType leftRT = DeserializeRecursive(newStrArray);
+                    ReturnType rightRT = DeserializeRecursive(leftRT.strArray);
+
+                    TreeNode curTreeNode = new TreeNode(int.Parse(rootStr));
+                    curTreeNode.left = leftRT.treeNode;
+                    curTreeNode.right = rightRT.treeNode;
+                    rt = new ReturnType(curTreeNode, rightRT.strArray);
+                }
+                else
+                {
+                    TreeNode curTreeNode = new TreeNode(int.Parse(rootStr));
+                    rt = new ReturnType(curTreeNode, null);
+                }
             }
             else
             {
-                rt = new ReturnType(null, null);
+                int newStrArrayLength = strArray.Length - 1;
+
+                if (newStrArrayLength > 0)
+                {
+                    string[] newStrArray = new string[strArray.Length - 1];
+                    for (int i = 1; i < strArray.Length; i++)
+                    {
+                        newStrArray[i - 1] = strArray[i];
+                    }
+                    //strArray.CopyTo(newStrArray, 1);
+                    rt = new ReturnType(null, newStrArray);
+                }
+                else
+                {
+                    rt = new ReturnType(null, null);
+                }
             }
 
 
