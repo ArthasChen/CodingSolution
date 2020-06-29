5851dc538b9de454e7670d016af7b4d057ed986c
sibo.chen <420171312@qq.com>
2020-06-29 17:28:44 +0800

    Q04_FindInPartiallySortedMatrix 二维数组中的查找 done

---
+++ Q04_FindInPartiallySortedMatrix/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q04_FindInPartiallySortedMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public bool FindNumberIn2DArray(int[][] matrix, int target)
+        {
+            if (matrix.Length == 0)
+            {
+                return false;
+            }
+
+            bool isFindOut = false;
+
+            int maxIndex = 0;
+            int xLength = matrix[0].Length;
+            int yLength = matrix.Length;
+
+            maxIndex = xLength;
+
+            for (int y = 0; y < yLength; y++)
+            {
+                for (int x = 0; x < maxIndex; x++)
+                {
+                    if (matrix[y][x] < target)
+                    {
+
+                    }
+                    else if (matrix[y][x] > target)
+                    {
+                        maxIndex = x;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return isFindOut;
+        }
+    }
+}
