963918427f5fd85046277150ab9bcbbe76daecbb
Arthas <chensibo@nuaa.edu.cn>
2020-06-30 00:00:15 +0800

    Q36_ConvertBinarySearchTree 二叉搜索树与双向链表 leetcode done

--- Q37_SerializeBinaryTrees/Program.cs
+++ Q37_SerializeBinaryTrees/Program.cs
@@ -28,6 +28,13 @@ namespace Q37_SerializeBinaryTrees
             SolutionNK s = new SolutionNK();
             var head = s.Serialize(node1);
 
+            Codec c = new Codec();
+            c.serialize(node1);
+
+            string aaa = "[1,2,3,4,null,null,7]";
+
+            c.deserialize(aaa);
+
             Console.ReadKey();
 
         }
@@ -56,113 +63,9 @@ namespace Q37_SerializeBinaryTrees
         }
     }
 
-    public class Codec
-    {
-        StringBuilder sb = new StringBuilder();
-        // Encodes a tree to a single string.
-        public string serialize(TreeNode root)
-        {
-            // write code here
-            if (root == null)
-            {
-                return "#!";
-            }
-
-            PreOrderTraversalWithRecursive(root);
-
-
-            return sb.ToString();
-        }
-
-        public void PreOrderTraversalWithRecursive(TreeNode root)
-        {
-            if (root == null)
-            {
-                sb.Append("#!");
-                return;
-            }
-
-            sb.Append(root.val);
-            sb.Append("!");
-            //Console.WriteLine(root.val.ToString() + "  ");
-            PreOrderTraversalWithRecursive(root.left);
-            PreOrderTraversalWithRecursive(root.right);
-        }
-
-        // Decodes your encoded data to tree.
-        public TreeNode deserialize(string data)
-        {
-            string[] inputCharArray = data.Split(new char[1] { '!' }, StringSplitOptions.RemoveEmptyEntries);
-
-            ReturnType resultTreeNode = null;
-
-            //str.
-            return DeserializeRecursive(inputCharArray).treeNode;
-        }
-
-        public ReturnType DeserializeRecursive(string[] strArray)
-        {
-            if (strArray == null)
-            {
-                return new ReturnType(null, null);
-            }
-
-
-            ReturnType rt = null;
-            string rootStr = strArray[0];
-            if (rootStr != "#")
-            {
-                int newStrArrayLength = strArray.Length - 1;
-
-                if (newStrArrayLength > 0)
-                {
-                    string[] newStrArray = new string[strArray.Length - 1];
-                    for (int i = 1; i < strArray.Length; i++)
-                    {
-                        newStrArray[i - 1] = strArray[i];
-                    }
-                    //strArray.CopyTo(newStrArray, 0);
-
-                    ReturnType leftRT = DeserializeRecursive(newStrArray);
-                    ReturnType rightRT = DeserializeRecursive(leftRT.strArray);
-
-                    TreeNode curTreeNode = new TreeNode(int.Parse(rootStr));
-                    curTreeNode.left = leftRT.treeNode;
-                    curTreeNode.right = rightRT.treeNode;
-                    rt = new ReturnType(curTreeNode, rightRT.strArray);
-                }
-                else
-                {
-                    TreeNode curTreeNode = new TreeNode(int.Parse(rootStr));
-                    rt = new ReturnType(curTreeNode, null);
-                }
-            }
-            else
-            {
-                int newStrArrayLength = strArray.Length - 1;
-
-                if (newStrArrayLength > 0)
-                {
-                    string[] newStrArray = new string[strArray.Length - 1];
-                    for (int i = 1; i < strArray.Length; i++)
-                    {
-                        newStrArray[i - 1] = strArray[i];
-                    }
-                    //strArray.CopyTo(newStrArray, 1);
-                    rt = new ReturnType(null, newStrArray);
-                }
-                else
-                {
-                    rt = new ReturnType(null, null);
-                }
-            }
-
-
-            return rt;
-        }
-    }
-
-
+    /// <summary>
+    /// 牛客的解题方法，牛客不限序列化的格式，我是按照前序遍历完成的。
+    /// </summary>
     class SolutionNK
     {
         StringBuilder sb = new StringBuilder();
@@ -267,5 +170,131 @@ namespace Q37_SerializeBinaryTrees
 
             return rt;
         }
+    }
+
+    /// <summary>
+    /// Leetcode的解题方法，leetcode规定了序列化必须用层序遍历
+    /// </summary>
+    public class Codec
+    {
+        StringBuilder sb = new StringBuilder();
+        // Encodes a tree to a single string.
+        public string serialize(TreeNode root)
+        {
+            // write code here
+            if (root == null)
+            {
+                return null;
+            }
+
+            // 层序遍历，没有就是null
+            var liststr = PrintFromTopToBottom(root);
+
+            StringBuilder sbb = new StringBuilder("[");
+            foreach (var child in liststr)
+            {
+                sbb.Append(child);
+                sbb.Append(',');
+            }
+
+            sbb.Replace(',', ']', sbb.Length - 1, 1);
+            return sbb.ToString();
+        }
+
+        public List<string> PrintFromTopToBottom(TreeNode root)
+        {
+            // write code here
+            List<string> resultList = new List<string>();
+
+            if (root == null)
+            {
+                return resultList;
+            }
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+                if (cur != null)
+                {
+                    resultList.Add(cur.val.ToString());
+                }
+                else
+                {
+                    resultList.Add("null");
+                }
+
+                if (cur != null)
+                {
+                    if (cur.left != null)
+                    {
+                        queue.Enqueue(cur.left);
+                    }
+                    else
+                    {
+                        queue.Enqueue(null);
+                    }
+
+                    if (cur.right != null)
+                    {
+                        queue.Enqueue(cur.right);
+                    }
+                    else
+                    {
+                        queue.Enqueue(null);
+                    }
+                }
+            }
+
+            //while (resultList.Last() == "null")
+            //{
+            //    resultList.RemoveAt(resultList.Count - 1);
+            //}
+
+            return resultList;
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(string data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            if (data.Equals("[]"))
+            {
+                return null;
+            }
+
+            var tem = data.TrimStart('[');
+            var tem2 = tem.TrimEnd(']');
+            string[] inputCharArray = tem2.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            TreeNode root = new TreeNode(int.Parse(inputCharArray[0]));
+            queue.Enqueue(root);
+            int i = 1;
+            while (queue.Count != 0)
+            {
+                TreeNode cur = queue.Dequeue();
+                if (!inputCharArray[i].Equals("null"))
+                {
+                    cur.left = new TreeNode(int.Parse(inputCharArray[i]));
+                    queue.Enqueue(cur.left);
+                }
+                i++;
+                if (!inputCharArray[i].Equals("null"))
+                {
+                    cur.right = new TreeNode(int.Parse(inputCharArray[i]));
+                    queue.Enqueue(cur.right);
+                }
+                i++;
+            }
+
+            return root;
+        }
+
     }
 }
