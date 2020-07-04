5a49de2b833b522d15eb3f17bf5b759fe2e10822
Arthas <chensibo@nuaa.edu.cn>
2020-07-04 23:04:45 +0800

    Q35_CopyComplexList 复杂链表的复制 添加两个新的优化算法 done

--- Q35_CopyComplexList/Program.cs
+++ Q35_CopyComplexList/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace Q35_CopyComplexList
 
             Node n11 = new Node(1);
 
-            Solution s = new Solution();
+            Solution3 s = new Solution3();
 
             var aaa = s.CopyRandomList(n1);
 
@@ -141,8 +142,6 @@ namespace Q35_CopyComplexList
                     curCopyNode.random = t;
                 }
 
-                
-
                 curCopyNode = curCopyNode.next;
                 curNode = curNode.next;
             }
@@ -152,9 +151,93 @@ namespace Q35_CopyComplexList
     }
 
     /// <summary>
-    /// 解法二：
+    /// 解法二：用哈希表存原始node和克隆node，这样可以用O(n)的空间把时间复杂度从O(n^2)降低到的O(n)。
+    /// 时间复杂度：O(n) 
+    /// 空间复杂度：O(n)
     /// </summary>
     public class Solution2
+    {
+        Dictionary<Node, Node> dic = new Dictionary<Node, Node>();
+
+        public Node CopyRandomList(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            Node cloneNodeHead = CopyBody(head);
+
+            Node resultNodeHead = CopyRadom(head, cloneNodeHead);
+
+            return resultNodeHead;
+        }
+
+        public Node CopyBody(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            Node currentNode = head;
+            Node preCloneNode = null;
+            Node cloneNodeHead = null;
+
+            while (currentNode != null)
+            {
+                Node cloneNode = new Node(currentNode.val);
+
+                // 原节点对应克隆节点，添加进哈希表
+                dic[currentNode] = cloneNode;
+
+                if (preCloneNode != null)
+                {
+                    preCloneNode.next = cloneNode;
+                }
+                else
+                {
+                    cloneNodeHead = cloneNode;
+                }
+
+                preCloneNode = cloneNode;
+                currentNode = currentNode.next;
+            }
+
+            return cloneNodeHead;
+        }
+
+        public Node CopyRadom(Node oriHead, Node copyNodeHead)
+        {
+            Node curOriNode = oriHead;
+            Node curCloneNode = copyNodeHead;
+
+            while (curOriNode != null)
+            {
+                if (curOriNode.random != null)
+                {
+                    curCloneNode.random = dic[curOriNode.random];
+                }
+                else
+                {
+                    curCloneNode.random = null;
+                }
+
+                curOriNode = curOriNode.next;
+                curCloneNode = curCloneNode.next;
+            }
+
+            return copyNodeHead;
+        }
+    }
+
+    /// <summary>
+    /// 解法三：第一次遍历原链表时，构建的新节点添加在原节点后，这样就形成了 “原节点1-新节点1-原节点2-新节点2。。。” 这样一个新的链表。
+    /// 在这个链表中对新节点添加Random节点的空间复杂度是O(n)，并且无需O(n)的额外空间。添加完Random节点后，分解链表，使原链表恢复，复制的链表拆分出来。整个算法遍历3次，时间复杂度是时间复杂度：O(n)。
+    /// 时间复杂度：O(n) 
+    /// 空间复杂度：O(1)
+    /// </summary>
+    public class Solution3
     {
         public Node CopyRandomList(Node head)
         {
@@ -163,9 +246,84 @@ namespace Q35_CopyComplexList
                 return null;
             }
 
-            return null;
+            // clone copy链表的next，构建链表
+            Node newNodeHead = CreateNewList(head);
+
+            // 通过链表中原节点的random来给clone链表的random赋值
+            newNodeHead = CloneRandomNode(newNodeHead);
+
+            // 分解链表
+            Node cloneNodeHead = DisconnectList(newNodeHead);
+
+            return cloneNodeHead;
         }
 
-        
+        private Node CreateNewList(Node head)
+        {
+            Node curNode = head;
+            Node curCloneNode = null;
+
+            while (curNode != null)
+            {
+                curCloneNode = new Node(curNode.val);
+                Node temNode = curNode.next;
+                curNode.next = curCloneNode;
+                curCloneNode.next = temNode;
+
+                curNode = temNode;
+            }
+
+            return head;
+        }
+
+        private Node CloneRandomNode(Node newNodeHead)
+        {
+            Node curNode = newNodeHead;
+
+            while (curNode != null)
+            {
+                if (curNode.random != null)
+                {
+                    curNode.next.random = curNode.random.next;
+                }
+                else
+                {
+                    curNode.next.random = null;
+                }
+
+
+                curNode = curNode.next.next;
+            }
+
+            return newNodeHead;
+        }
+
+        private Node DisconnectList(Node newNodeHead)
+        {
+            int counts = 1;
+
+            Node curOriNode = newNodeHead;
+            Node curCloneNode = newNodeHead.next;
+            Node curCloneHead = newNodeHead.next;
+
+            while (curOriNode != null)
+            {
+                Node temNode = curCloneNode.next;
+                curOriNode.next = temNode;
+                if (temNode != null)
+                {
+                    curCloneNode.next = temNode.next;
+                }
+                else
+                {
+                    curCloneNode.next = null;
+                }
+
+                curOriNode = curOriNode.next;
+                curCloneNode = curCloneNode.next;
+            }
+
+            return curCloneHead;
+        }
     }
 }
