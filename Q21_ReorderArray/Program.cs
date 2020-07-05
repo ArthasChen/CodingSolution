2c693063af86a9c5efed580786ee70cdc19333d1
Arthas <chensibo@nuaa.edu.cn>
2020-07-05 23:42:56 +0800

    Q21_ReorderArray 调整数组顺序使奇数位于偶数前面   done

---
+++ Q21_ReorderArray/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q21_ReorderArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ss = new int[] { 1,6,6,6,6,9,2,3,4};
+            Solution s=new Solution();
+            var a = s.Exchange(ss);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] Exchange(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return new int[0];
+            }
+
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+            bool leftIsOdd = false;
+            bool rightIsEven = false;
+
+            while (leftIndex < rightIndex)
+            {
+                leftIsOdd = (nums[leftIndex] % 2) == 1 ? true : false;
+                rightIsEven = (nums[rightIndex] % 2) == 0 ? true : false;
+
+
+                // 左边是奇数 右边是偶数 分别右移 左移
+                if (leftIsOdd && rightIsEven)
+                {
+                    leftIndex++;
+                    rightIndex--;
+                }
+                // 左边是奇数 右边是奇数 左移 右不动
+                else if (leftIsOdd && !rightIsEven)
+                {
+                    leftIndex++;
+                }
+                // 左边是偶数 右边是偶数 右移 左不动
+                else if (!leftIsOdd && rightIsEven)
+                {
+                    rightIndex--;
+                }
+                // 左边是偶数 右边是奇数 对换 然后分别右移 左移
+                else
+                {
+                    int tem = nums[rightIndex];
+                    nums[rightIndex] = nums[leftIndex];
+                    nums[leftIndex] = tem;
+
+                    leftIndex++;
+                    rightIndex--;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
