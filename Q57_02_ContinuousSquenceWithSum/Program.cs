6796801a0c52870bcd014ffebe04e47e32b06389
Arthas <chensibo@nuaa.edu.cn>
2020-07-08 23:52:40 +0800

    Q57_02_ContinuousSquenceWithSum 和为s的连续正数序列 done

---
+++ Q57_02_ContinuousSquenceWithSum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q57_02_ContinuousSquenceWithSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.FindContinuousSequence(9);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int[][] FindContinuousSequence(int target)
+        {
+            int[][] result = new int[0][];
+            if (target <= 2)
+            {
+                return result;
+            }
+
+            List<int[]> list = new List<int[]>();
+
+            for (int i = 1; i <= target / 2; i++)
+            {
+                int num = i;
+                bool isContinue = true;
+                int sum = 0;
+                int lastNum = 0;
+                while (isContinue)
+                {
+                    sum += num;
+                    if (sum < target)
+                    {
+                        num++;
+                    }
+                    else if (sum == target)
+                    {
+                        lastNum = num;
+                        isContinue = false;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (lastNum != 0)
+                {
+                    int[] addArray = new int[lastNum - i + 1];
+                    for (int j = i; j <= lastNum; j++)
+                    {
+                        addArray[j - i] = j;
+                    }
+                    list.Add(addArray);
+                }
+            }
+
+            result = new int[list.Count][];
+            for (int i = 0; i < list.Count; i++)
+            {
+                result[i] = list[i];
+            }
+
+            return result;
+        }
+    }
+}
