699b63be683f8c8234cca0fb56c26c09b2866d65
Arthas <chensibo@nuaa.edu.cn>
2020-07-10 23:01:52 +0800

    Q53_02_MissingNumber 0～n-1中缺失的数字 done

---
+++ Q53_02_MissingNumber/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q53_02_MissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {0, 1, 3};
+            Solution s= new Solution();
+            int v= s.MissingNumber(a);
+        }
+    }
+
+    /// <summary>
+    /// 时间复杂度：O(logn)
+    /// 空间复杂度：O(1)
+    /// </summary>
+    public class Solution
+    {
+        public int MissingNumber(int[] nums)
+        {
+            if (nums.Length <= 0)
+            {
+                return 0;
+            }
+
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+
+            while (leftIndex <= rightIndex)
+            {
+                int midIndex = (leftIndex + rightIndex) >> 1;
+                if (nums[midIndex] != midIndex)
+                {
+                    if (midIndex == 0 || nums[midIndex - 1] == (midIndex - 1))
+                    {
+                        return midIndex;
+                    }
+
+                    rightIndex = midIndex - 1;
+                }
+                else
+                {
+                    leftIndex = midIndex + 1;
+                }
+
+            }
+
+            if (leftIndex == nums.Length)
+            {
+                return nums.Length;
+            }
+
+
+            return -1;
+        }
+    }
+}
