73185eaf4d8072d0ea64b5ba00df034f583ca205
Arthas <chensibo@nuaa.edu.cn>
2020-07-15 22:23:57 +0800

    Q65_AddTwoNumbers 不用加减乘除做加法 done

---
+++ Q65_AddTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q65_AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s= new Solution();
+            var re= s.Add(15, 6);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public int Add(int a, int b)
+        {
+            int sum = a;
+            int tem = b;
+            int lastSum =0;
+            int lastTem = 1;
+            while (tem != 0)
+            {
+                lastSum = sum ^ tem;
+                lastTem = (sum & tem) << 1;
+
+                tem = (lastSum & lastTem) << 1;
+                sum = lastSum ^ lastTem;
+            }
+
+            return sum;
+        }
+    }
+}
