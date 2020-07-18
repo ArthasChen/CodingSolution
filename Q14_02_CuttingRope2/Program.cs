403b98942c3e9dc584ffb2994470bcb5039e80a8
Arthas <chensibo@nuaa.edu.cn>
2020-07-18 22:26:13 +0800

    Q14_02_CuttingRope2 剪绳子2 done

---
+++ Q14_02_CuttingRope2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q14_02_CuttingRope2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /// <summary>
+    /// 剪绳子2：在上一题的基础上，增加n的取值范围，因此题目就涉及到到大数问题。
+    /// </summary>
+    public class Solution
+    {
+        public int CuttingRope(int n)
+        {
+            {
+                if (n == 2)
+                {
+                    return 1;
+                }
+                if (n == 3)
+                {
+                    return 2;
+                }
+                int mod = (int)1e9 + 7;
+                long res = 1;
+                while (n > 4)
+                {
+                    res *= 3;
+                    res %= mod;
+                    n -= 3;
+                }
+                return (int)(res * n % mod);
+            }
+        }
+    }
+}
