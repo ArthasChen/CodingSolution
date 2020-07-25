c414edde9b190547d602ef47522b351a15bedb92
Arthas <chensibo@nuaa.edu.cn>
2020-07-25 13:23:25 +0800

    Q44_DigitsInSequence 数字序列中某一位的数字 添加新方法

--- Q44_DigitsInSequence/Program.cs
+++ Q44_DigitsInSequence/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,15 @@ namespace Q44_DigitsInSequence
     {
         static void Main(string[] args)
         {
+            Solution s = new Solution();
+            var bb = s.getNums(11);
+            var a = s.FindNthDigit(1000000000);
+            var qq= (long)Math.Pow(10, 11);
+            Console.ReadKey();
         }
     }
 
-    public class Solution
+    public class Solution2
     {
         public int FindNthDigit(int n)
         {
@@ -44,4 +50,106 @@ namespace Q44_DigitsInSequence
             return value.ToString()[(int)remainder] - '0';
         }
     }
+
+
+
+
+
+    public class Solution
+    {
+        public int FindNthDigit(int n)
+        {
+            if (n < 0)
+            {
+                return -1;
+            }
+
+            long targetbit = n;
+            int baseNumber = 10;// 10进制基数
+            long nums = 10;// 当前位数的数字共有几个
+            long bitNums = 1;// 表示位数 从1位开始
+
+            while (targetbit > nums * bitNums)
+            {
+                targetbit -= bitNums * nums;
+
+                bitNums++;
+                nums = getNums(bitNums);
+
+            }
+
+            int a = (int)(targetbit / bitNums);
+            int b = (int)(targetbit % bitNums);
+
+            long lastNumber = (long)Math.Pow(10, bitNums) - nums + a;
+            string tem = lastNumber.ToString();
+
+            long resulet = long.Parse(tem[b].ToString());
+
+
+            return (int)resulet;
+        }
+
+        public long getNums(long bitNums)
+        {
+            if (bitNums==1)
+            {
+                return 10;
+            }
+
+            long count = (long)(Math.Pow(10, bitNums - 1));
+
+            return 9 * count;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
