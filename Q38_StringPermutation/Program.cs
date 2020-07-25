1fc82d48c22e2d0e3b3a603a9e7c0340af49e51c
Arthas <chensibo@nuaa.edu.cn>
2020-07-25 19:39:13 +0800

    Q38_StringPermutation 字符串的排列 done

--- Q38_StringPermutation/Program.cs
+++ Q38_StringPermutation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,69 @@ namespace Q38_StringPermutation
     {
         static void Main(string[] args)
         {
+            string input = "aab";
+            Solution s = new Solution();
+            var v = s.Permutation(input);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        private char[] charOfString;
+        private List<string> resultList;
+        private HashSet<string> resultSet;
+
+        public string[] Permutation(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return new string[] { "" };
+            }
+
+            charOfString = s.ToCharArray();
+            resultList = new List<string>();
+            resultSet = new HashSet<string>();
+
+            string[] strArray = null;
+
+            PermutationCore(0);
+
+            string[] resultArray = resultList.ToArray();
+
+            return resultArray;
+        }
+
+        public void PermutationCore(int index)
+        {
+            if (index == charOfString.Length)
+            {
+                if (!resultSet.Contains(new string(charOfString)))
+                {
+                    resultSet.Add(new string(charOfString));
+                    resultList.Add(new string(charOfString));
+                    Debug.Print(charOfString.ToString());
+                }
+                return;
+            }
+
+            for (int i = index; i < charOfString.Length; i++)
+            {
+
+                Swap(index, i);
+
+                PermutationCore(index + 1);
+
+                Swap(index, i);
+            }
+        }
+
+        public void Swap(int i, int j)
+        {
+            char tem = charOfString[i];
+            charOfString[i] = charOfString[j];
+            charOfString[j] = tem;
         }
     }
 }
